dae77e9216b086fc336ad0830784978ec0cfbe2a
Sahil Hossain <sahilthegeek999@gmail.com>
2024-11-01 09:48:35 +0530

    First Commit

--- app/src/main/java/com/pegalite/threegonly/MainActivity.java
+++ app/src/main/java/com/pegalite/threegonly/MainActivity.java
@@ -24,9 +24,9 @@ import com.pegalite.threegonly.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     ActivityMainBinding binding;
     private InterstitialAd mInterstitialAd;
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
